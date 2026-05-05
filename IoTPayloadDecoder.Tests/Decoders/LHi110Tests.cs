fd154ded82785cd84b9d41ab2aab5ad05e84bb51
Gustav Pihlquist <gustav.pihlquist@kungsbacka.se>
2026-05-05 16:43:38 +0200

    Rearrange project structure and add tests

---
+++ IoTPayloadDecoder.Tests/Decoders/LHi110Tests.cs
@@ -0,0 +1,55 @@
+﻿using IoTPayloadDecoder.Decoders.LHi110;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+using IoTPayloadDecoder.Tests.TestHelpers;
+
+namespace IoTPayloadDecoder.Tests.Decoders
+{
+    public class LHi110Tests
+    {
+        private readonly ITestOutputHelper _output;
+
+        private readonly IPayloadDecoder _port1ProtocolDataDecoder = 
+            DecoderFactory.Create(DeviceModel.LHi110, 1);
+
+        private readonly IPayloadDecoder _port2PeriodicDataDecoder = 
+            DecoderFactory.Create(DeviceModel.LHi110, 2);
+
+        public LHi110Tests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void Decode_Port2MessageFormat1_ShouldDecodeExcpetedValues()
+        {
+            var payload =
+            "016774858000000186A00064005000C800A0012C00F0398E739A00A0501E";
+
+            dynamic result = _port2PeriodicDataDecoder.Decode(payload, compact: false);
+
+            Assert.Equal(1, result.messageFormat.value);
+            Assert.Equal((ulong)100000, result.activeImportReading.value);
+            Assert.Empty(result.warnings);
+        }
+
+        [Fact]
+        public void Decode_Port1CpuVoltage_ShouldDecodeExpectedValue()
+        {
+            var payload = "01060BB8";
+
+            dynamic result = _port1ProtocolDataDecoder.Decode(payload, compact: false);
+
+            JsonTestOutput.PrintResult(_output, result);
+
+            Assert.Equal(3000, result.cpuVoltage.value);
+            Assert.Empty(result.warnings);
+
+        }
+    }
+}
